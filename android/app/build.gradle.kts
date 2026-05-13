ba57dc9a9aea8fda29739aac05dfca4d96f9d8b3
wanhongbo <70862340@qq.com>
2026-05-13 19:51:18 +0800

    version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.xpx.vault"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 4
+        versionName = "0.2.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
