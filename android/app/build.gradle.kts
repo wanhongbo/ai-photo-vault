5fc100efad74d022d34289479202e638ca867e7a
万红波 <70862340@qq.com>
2026-05-28 17:02:36 +0800

    Bump Android version to 0.2.6

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.xpx.vault"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.2.5"
+        versionCode = 9
+        versionName = "0.2.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
