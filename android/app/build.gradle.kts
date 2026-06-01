be9279620c204bf6536faa4e651d6768c926ee97
万红波 <70862340@qq.com>
2026-06-01 16:32:02 +0800

    Bump Android release version to 0.2.8

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.xpx.vault"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.2.7"
+        versionCode = 11
+        versionName = "0.2.8"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
