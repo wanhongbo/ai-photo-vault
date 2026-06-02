918249e57c4943772369602c37d5cafcac760a5c
万红波 <70862340@qq.com>
2026-06-02 10:57:30 +0800

    Bump Android version to 0.2.9

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.xpx.vault"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "0.2.8"
+        versionCode = 12
+        versionName = "0.2.9"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
